9611dfce9acc7b558d3df72924c715fd5d2dcbf9
sophie homer <sophiehomer94@gmail.com>
2022-05-05 15:43:22 -0700

    added email input to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -9,6 +9,9 @@ const Contact = () => {
           <form className="contactForm">
             <label for="contact-name">Name</label>
             <input type="text" className="contactName" id="contact-name" />
+
+            <label for="contact-email">Email</label>
+            <input type="text" className="contactEmail" id="contact-Email" />
         
             <label for="contact-message">Message</label>
             <textarea id="contact-message" className="contactMessage"></textarea>
