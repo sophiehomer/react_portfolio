0b75d4e1ea27ef4da9c511918f1db9190bb2a36f
sophie homer <sophiehomer94@gmail.com>
2022-05-15 11:29:12 -0700

    fixed footer, added and changed portfolio styles

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,15 +6,9 @@ const Contact = () => {
     <>
     <div className="contactForm">
           <form className="contactForm">
-            {/* <label for="contact-name">Name</label> */}
             <input type="text" className="contactName" id="contact-name" placeholder="YOUR NAME"/>
-            
-            {/* <label for="contact-email">Email</label> */}
             <input type="text" className="contactEmail" id="contact-Email" placeholder="EMAIL ADDRESS"/>
-        
-            {/* <label for="contact-message">Message</label> */}
             <textarea id="contact-message" className="contactMessage" placeholder="MESSAGE"></textarea>
-        
             <button className="submitButton" type="submit">SUBMIT</button>
           </form>
         </div>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -11,34 +11,26 @@ const Portfolio = () => {
   return (
     <>
     <div className="portfolioImages">
-    <h2 className="project">
-      <a href="https://github.com/kpbader/kit" className='githubIcon'><FaGithub size={25}/></a> 
-      SOCIAL APPLICATION
-    </h2>
+      
     <a href="https://blooming-anchorage-21473.herokuapp.com/">
       <img src={kitImage} className="kitImg" style={{ width: "40%" }} alt="kit"/>
     </a>
-    <h2 className="project">
-      <a href="https://github.com/Abarragan89/dinner-drinks" className='githubIcon'><FaGithub size={25}/></a>
-      RECIPE AND COCKTAIL SEARCH ENGINE
-    </h2>
+    <a href="https://github.com/kpbader/kit" className='gitHubLink'> SOCIAL APPLICATION <FaGithub size={15}/></a> 
+    
     <a href="https://abarragan89.github.io/dinner-drinks/">
       <img src={dinnerDrinksImage} className="dinnerDrinksImg" style={{ width: "40%" }} alt="dinnerDrinks"/>
     </a>
-    <h2 className="project">
-      <a href="https://github.com/sophiehomer/Weather-Dashboard" className='githubIcon'><FaGithub size={25}/></a>
-       WEATHER APPLICATION
-    </h2>
+    <a href="https://github.com/Abarragan89/dinner-drinks" className='gitHubLink'> RECIPE AND COCKTAIL SEARCH ENGINE <FaGithub size={15}/></a>
+
     <a href="https://sophiehomer.github.io/Weather-Dashboard/" >
       <img src={weatherDashboardImage} className="weatherDashboardImg" style={{ width: "40%" }} alt="weatherDashboard"/>
     </a>
-    <h2 className="project">
-      <a href="https://github.com/sophiehomer/work-day-scheduler" className='githubIcon'><FaGithub size={25}/></a>
-      WORK DAY SCHEDULER
-    </h2> 
+    <a href="https://github.com/sophiehomer/Weather-Dashboard" className='gitHubLink'> WEATHER APPLICATION <FaGithub size={15}/></a>
+
     <a href="https://sophiehomer.github.io/work-day-scheduler/" >
       <img src={workDaySchedulerImage} className="workDaySchedulerImg" style={{ width: "40%" }} alt="workDayScheduler"/>
     </a>
+    <a href="https://github.com/sophiehomer/work-day-scheduler" className='workDaySchedulerLink'> WORK DAY SCHEDULER <FaGithub size={15}/></a>
     </div>
     </>
   )
