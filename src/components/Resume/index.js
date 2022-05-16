5c7e394fafa4367548d6b135f6ceb4897b7b40d1
sophie homer <sophiehomer94@gmail.com>
2022-05-15 19:18:44 -0700

    styles

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,13 +7,12 @@ const Resume = () => {
     {/* <div className="pageTitle">RESUME</div> */}
       <h1 className="frontEnd">FRONT-END PROFICIENCIES</h1>
         <p className="frontEndList">
-          HTML | CSS | JavaScript | jQuery | React | Responsive Design | Bootstrap
+          HTML CSS JavaScript jQuery React Responsive Design Bootstrap
         </p>  
       <h2 className="backEnd">BACK-END PROFICIENCIES</h2>
         <p className="backEndList">
-        APIs | Node | Express | MySQL | Sequelize | MongoDB | Mongoose | REST | GraphQL
+        APIs Node Express MySQL Sequelize MongoDB Mongoose REST GraphQL
         </p>
-
       <h3 className="downloadResume">DOWNLOAD MY RESUME</h3>     
     </>
   )
