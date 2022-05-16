ab8602b68d3b894952b3def5d3714cc6292a84aa
sophie homer <sophiehomer94@gmail.com>
2022-05-15 21:37:45 -0700

    email js and email functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,64 @@
-import React from 'react'
-import './styles.css'
+import { validateEmail } from '../../utils/helpers';
+import { useState, useRef } from 'react';
+import emailjs from 'emailjs-com';
+import './styles.css';
 
 const Contact = () => {
+  const [ formState, setFormState] = useState({name: '', email: '', message: ''})
+  const { name, email, message } = formState
+  const [errorMessage, setErrorMessage] = useState('');
+  // handle error handling and on change events in the form elements
+  function handleChange(e) {
+      if(e.target.name === 'email') {
+          const isValid = validateEmail(e.target.value);
+          if(!isValid) {
+              setErrorMessage('Invalid email address')
+          } else {
+              setErrorMessage('');
+          }
+      } else {
+          if(!e.target.value.length) {
+              setErrorMessage(`${e.target.name} required`)
+          } else {
+              setErrorMessage('');
+          }
+      }
+      setFormState({...formState, [e.target.name]: e.target.value})
+      if (!errorMessage) {
+          setFormState({...formState, [e.target.name]: e.target.value})
+      }
+  }
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6j9wkzn', 'template_3i2ejak', form.current, 'UXp81vn8aAVHrC0RE')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
+
   return (
     <>
     <div className="contactForm">
-          <form className="contactForm">
-            <input type="text" className="contactName" id="contact-name" placeholder="YOUR NAME"/>
-            <input type="text" className="contactEmail" id="contact-Email" placeholder="EMAIL ADDRESS"/>
-            <textarea id="contact-message" className="contactMessage" placeholder="MESSAGE"></textarea>
+          <form ref={form} onSubmit={sendEmail} className="contactForm">
+            <input type="text" name="name" defaultValue={name} className="contactName" id="contact-name"  onBlur={handleChange} placeholder="YOUR NAME"/>
+            <input type="text"  name="email" defaultValue={email} className="contactEmail" id="contact-Email" onBlur={handleChange} placeholder="EMAIL ADDRESS"/>
+            <textarea name="Message" id="contact-message" defaultValue={message} className="contactMessage" onBlur={handleChange} placeholder="MESSAGE"></textarea>
             <button className="submitButton" type="submit">SUBMIT</button>
+
+            {errorMessage && (
+                    <div>
+                        <p className="errorMessage">{errorMessage}</p>
+                    </div>
+                )}
+             
+              
           </form>
         </div>
     </>
