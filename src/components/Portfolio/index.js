0a1ccd3afe829c7db9655937d13408c6daf78970
sophie homer <sophiehomer94@gmail.com>
2022-08-04 12:39:14 -0700

    updated portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import './styles.css'
 import femmeDept from '../../assets/portfolio/femme_dept.png'
-// import kitImage from '../../assets/portfolio/kit-img.png'
-// import weatherDashboardImage from '../../assets/portfolio/weather-dashboard.png'
-// import workDaySchedulerImage from '../../assets/portfolio/work-day-scheduler.png'
-// import drawTheLineImage from '../../assets/portfolio/draw-the-line.png'
-import kit from '../../assets/portfolio/kitLaptop.jpeg'
+import kit from '../../assets/portfolio/kit.png'
+import dinnerDrinks from '../../assets/portfolio/dinner_drinks.png'
 import { FaGithub } from 'react-icons/fa';
 import { AiOutlineArrowRight } from 'react-icons/ai'
 
@@ -16,37 +13,7 @@ const Portfolio = () => {
     <>
     <main className="portfolioImages" id="portfolioImages">
 
-    <section className="kitContainer">
-        <div className="app">
-          <a href="https://drawtheline2022.herokuapp.com/"  rel="noreferrer" target="_blank">
-            <img src={kit} className="kitImg" alt="drawTheLine"/> 
-          </a>
-        </div>
-      <div className="appSummary">
-          <p className="appName">
-            KIT Social Application
-          </p>
-          <p className="role">
-            Role: Front-end developer, UX/UI
-          </p>
-          <p className="technologiesUsed">
-            Built with React, JavaScript, Node.js, Apollo, Mongoose, mongoDb, Bycrypt.
-          </p>
-          <p className="summary">
-            This application was developed for the aulmni of my coding bootcamp class. Users can post, comment, like and dislike. The application served as a communication network for mutual support within the class.                     
-          </p>
-          <div className="githubAndWebsiteIcon">
-            <p className="openWebsite">
-              <a href="https://drawtheline2022.herokuapp.com/"  className='gitHubLinkRight' rel="noreferrer" target="_blank">Application<AiOutlineArrowRight className="arrow" size={20}/></a>
-            </p>
-            <p className="openGithub"> 
-              <a href="https://github.com/Abarragan89/draw-the-line" className='gitHubLinkRight'  rel="noreferrer" target="_blank">GitHub <FaGithub size={22} className="githubLink"/></a> 
-            </p>
-        </div>
-      </div>
-      </section>
-
-      <section className="femmeDeptContainer">
+    <section className="femmeDeptContainer">
       <div className="app">
           <a href="https://www.figma.com/proto/sa327Zy6gLR6vb7wJH3fb1/Femme-Dept.?page-id=0%3A1&node-id=38%3A3576&viewport=363%2C242%2C0.02&scaling=scale-down&starting-point-node-id=38%3A3576"  rel="noreferrer" target="_blank">
             <img src={femmeDept} className="femmeDeptImg" alt="femmeDept"/>
@@ -68,6 +35,66 @@ const Portfolio = () => {
           <div className="githubAndWebsiteIcon">
             <p className="openWebsite">
               <a href="https://www.figma.com/proto/sa327Zy6gLR6vb7wJH3fb1/Femme-Dept.?page-id=0%3A1&node-id=38%3A3576&viewport=363%2C242%2C0.02&scaling=scale-down&starting-point-node-id=38%3A3576"  className='gitHubLinkRight' rel="noreferrer" target="_blank">Prototype <AiOutlineArrowRight className="arrow" size={20}/></a>
+            </p>
+        </div>
+      </div>
+      </section>
+
+    <section className="kitContainer">
+        <div className="app">
+          <a href="https://drawtheline2022.herokuapp.com/"  rel="noreferrer" target="_blank">
+            <img src={kit} className="kitImg" alt="drawTheLine"/> 
+          </a>
+        </div>
+      <div className="appSummary">
+          <p className="appName">
+            KIT Social Application
+          </p>
+          <p className="role">
+            Role: Front-end developer, UX/UI
+          </p>
+          <p className="technologiesUsed">
+            Built with React, JavaScript, Node.js, Apollo, Mongoose, mongoDb, Bycrypt.
+          </p>
+          <p className="summary">
+            An application developed for the aulmni of my coding bootcamp class. Users can post, comment, like and dislike. The application served as a communication network for mutual support within the class.                     
+          </p>
+          <div className="githubAndWebsiteIcon">
+            <p className="openWebsite">
+              <a href="https://drawtheline2022.herokuapp.com/"  className='gitHubLinkRight' rel="noreferrer" target="_blank">Application<AiOutlineArrowRight className="arrow" size={20}/></a>
+            </p>
+            <p className="openGithub"> 
+              <a href="https://github.com/Abarragan89/draw-the-line" className='gitHubLinkRight'  rel="noreferrer" target="_blank">GitHub <FaGithub size={22} className="githubLink"/></a> 
+            </p>
+        </div>
+      </div>
+      </section>
+
+      <section className="dinnerDrinksContainer">
+      <div className="app">
+          <a href="https://www.figma.com/proto/sa327Zy6gLR6vb7wJH3fb1/Femme-Dept.?page-id=0%3A1&node-id=38%3A3576&viewport=363%2C242%2C0.02&scaling=scale-down&starting-point-node-id=38%3A3576"  rel="noreferrer" target="_blank">
+            <img src={dinnerDrinks } className="dinnerDrinksImg" alt="dinnerDrinks"/>
+          </a>
+        </div>
+      <div className="appSummary">
+          <p className="appName">
+            Dinner & Drinks
+          </p>
+          <p className="role">
+            Role: Developer, UX UI.
+          </p>
+          <p className="technologiesUsed">
+            Built with HTML, CSS, Spoonacular API, Cocktail DB API.
+          </p>
+          <p className="summary">
+            A food and cocktail recipe application using the Spoonacular API and the Cocktails DB API to deliver curated recipes around user preferences.                  
+          </p>
+          <div className="githubAndWebsiteIcon">
+            <p className="openWebsite">
+              <a href="https://drawtheline2022.herokuapp.com/"  className='gitHubLinkRight' rel="noreferrer" target="_blank">Application<AiOutlineArrowRight className="arrow" size={20}/></a>
+            </p>
+            <p className="openGithub"> 
+              <a href="https://github.com/sophiehomer/dinner_and_drinks" className='gitHubLinkRight'  rel="noreferrer" target="_blank">GitHub <FaGithub size={22} className="githubLink"/></a> 
             </p>
         </div>
       </div>
