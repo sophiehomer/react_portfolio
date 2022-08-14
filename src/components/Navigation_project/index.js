62b37495e1eee28451ba17fd860750c0c20771ac
sophie homer <sophiehomer94@gmail.com>
2022-08-14 11:47:13 -0700

    updated svgs

--- src/components/Navigation_project/index.js
+++ src/components/Navigation_project/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
 import Pdf from '../../assets/documents/sophie-homer-resume.pdf'
 import { HashLink as Link } from 'react-router-hash-link';
 
