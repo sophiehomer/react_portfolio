7ba7a9cf281a01878da37b1f1f65a5b7c9e3e21c
sophie homer <sophiehomer94@gmail.com>
2022-08-14 15:35:49 -0700

    styled kit page

--- src/pages/kit/index.js
+++ src/pages/kit/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavProject from '../../components/Navigation_project';
+import kit from '../../../src/assets/kit/kit.svg'
 
 const Kit = () => {
   return (
@@ -7,23 +8,10 @@ const Kit = () => {
     <NavProject/>
     <h1 id="projectTitle" className="projectTitle">KIT</h1>
     <div className="kitPage">
-        
         <section className="kitSummaryContainer">
           <p className="kitSummary">
             A social application developed for the aulmni of my coding bootcamp class. Users can post, comment, like and dislike. The application served as a communication network for mutual support within the class.                     
           </p>
-          <section className="gitHubWebsiteContainer">
-            <div className="githubAndWebsite">
-              <a href="https://drawtheline2022.herokuapp.com/"  rel="noreferrer" target="_blank">
-                <button className="website">
-                  VIEW APP
-                </button>
-              </a>
-              <p className="gitHub"> 
-                <a href="https://github.com/sophiehomer/kit" className='gitHubLink'  rel="noreferrer" target="_blank">GitHub</a> 
-              </p>
-            </div>
-          </section>
           <section className="kitRoleAndTechnology">
             <div>
               <p className="myRole">
@@ -49,6 +37,23 @@ const Kit = () => {
             </div>
           </section>
         </section>
+
+
+        <section className="gitHubWebsiteContainer">
+        <section className="kitImageSection">
+          <img src={kit} className="kitImage" alt="femmeDept"/>
+        </section>
+            <div className="githubAndWebsite">
+              <a href="https://drawtheline2022.herokuapp.com/"  rel="noreferrer" target="_blank">
+                <button className="website">
+                  Open App
+                </button>
+              </a>
+              <p className="gitHub"> 
+                <a href="https://github.com/sophiehomer/kit" className='gitHubLink'  rel="noreferrer" target="_blank">GitHub</a> 
+              </p>
+            </div>
+          </section>
     </div>
     </>
   
