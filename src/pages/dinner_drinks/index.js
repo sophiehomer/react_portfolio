d14975fedeef1cad6713a1beb522d668d0012459
sophie homer <sophiehomer94@gmail.com>
2022-08-14 16:54:47 -0700

    cleaned up code

--- src/pages/dinner_drinks/index.js
+++ src/pages/dinner_drinks/index.js
@@ -9,25 +9,25 @@ const DinnerDrinks = () => {
   return (
     <>
      <NavProject/>
-    <h1 className="projectTitle">Dinner & Drinks</h1>
-    <div className="kitPage">
-        <section className="kitSummaryContainer">
-          <p className="kitSummary">
+      <h1 className="projectTitle">Dinner & Drinks</h1>
+      <div className="dinnerDrinkPage">
+        <section className="dinnerDrinkSummaryContainer">
+          <p className="dinnerDrinkSummary">
           This application generates curated recipes around user preferences. The user can search up food recipes based on cuisine and dietary restrictions, as well as look up cocktails recipes based on alcohol preference. The user has the ability to favorite recipes which are displayed on the users favorites page.
           </p>
-          <section className="kitRoleAndTechnology">
+          <section className="dinnerDrinkRoleAndTechnology">
             <div>
               <p className="myRole">
                 MY ROLE
               </p>
-              <p className="kitRole">
+              <p className="dinnerDrinkRole">
                 Front-end developer <br></br> UX/UI
               </p>
             </div>
             <div>
-              <p className="kitTechnology">
+              <p className="dinnerDrinkTechnology">
                 BUILT WITH
-                <p className="figma">
+                <p className="dinnerDrinkTechnologies">
                   Javascript <br></br> HTML <br></br> CSS <br></br> Spoonacular API <br></br> Cocktails DB API
                 </p>
               </p>
@@ -37,9 +37,9 @@ const DinnerDrinks = () => {
 
 
         <section className="gitHubWebsiteContainer">
-        <section className="kitImageSection">
-          <img src={dinnerDrinks} className="kitImage" alt="femmeDept"/>
-        </section>
+          <section className="kitImageSection">
+            <img src={dinnerDrinks} className="dinnerDrinkImage" alt="Dinner and Drinks"/>
+          </section>
             <div className="githubAndWebsite">
               <a href="https://sophiehomer.github.io/dinner_and_drinks/"  rel="noreferrer" target="_blank">
                 <button className="website">
@@ -47,11 +47,11 @@ const DinnerDrinks = () => {
                 </button>
               </a>
               <p className="gitHub"> 
-              <a href="https://github.com/sophiehomer/dinner_and_drinks" className='gitHubLink'  rel="noreferrer" target="_blank">GitHub</a> 
+                <a href="https://github.com/sophiehomer/dinner_and_drinks" className='gitHubLink'  rel="noreferrer" target="_blank">GitHub</a> 
               </p>
             </div>
           </section>
-    </div>
+      </div>
     </>
   
   )
