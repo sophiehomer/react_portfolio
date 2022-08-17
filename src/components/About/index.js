d37d131870f98f742a3254212ec84eed989f67bd
sophie homer <sophiehomer94@gmail.com>
2022-08-17 13:19:43 -0700

    updated about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,9 +9,10 @@ const About = () => {
   return (
     <section className="aboutContent" id="aboutContent">
       <div className="aboutTextDiv">
-      <h1 className="introText">Hi, I'm Sophie. I'm a full stack developer and my passion is product design. </h1>
+      <h1 className="title">Sophie Homer </h1>
+      <h2 className="introText">Full stack developer with a passion for product design.</h2>
       <p className="aboutText">
-          I find joy in the tension between form versus function, the big picture versus the details and old versus new. Previously, I worked in costume design for film and television, studying the art of visual storytelling. On my free time, I love to cook, curate playlists, catch up on my favorite shows and dabble in pottery.
+        Previously, I worked in costume design where I discovered a love of visual storytelling. Now, storytelling is what drives my product design. In my work, I strive to find balance between form and function, focusing on detail while never letting the broader picture out of sight. I find joy in making things both beautiful and easy to use. In my free time, I like to cook, curate playlists, and catch up on my favorite shows.
         </p>
       </div>
       <Navigation/>
