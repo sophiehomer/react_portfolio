a2216c1815c29f5acadbd4f39623b79785a16a0e
sophie homer <sophiehomer94@gmail.com>
2022-08-23 14:51:20 -0700

    fixed typo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,7 +10,7 @@ const About = () => {
     <section className="aboutContent" id="aboutContent">
       <div className="aboutTextDiv">
       <h1 className="title">Sophie Homer </h1>
-      <h2 className="introText">Full stack developer with a  <br></br> passion for product design.</h2>
+      <h2 className="introText">Full-stack developer with a  <br></br> passion for product design.</h2>
       <p className="aboutText">
         Previously, I worked in costume design where I discovered a love of visual storytelling. Now, storytelling is what inspires my product design. In my work, I strive to find balance between form and function, focusing on detail while never letting the broader picture out of sight. I find joy in making things both beautiful and easy to use. In my free time, I like to cook, curate playlists, and catch up on my favorite shows.
         </p>
