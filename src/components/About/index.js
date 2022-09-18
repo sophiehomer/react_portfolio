d103bb85a51d19d76e5579f96b5514cdd6c34bda
sophie homer <sophiehomer94@gmail.com>
2022-09-18 12:26:33 -0700

    updated about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,8 +12,7 @@ const About = () => {
       <h1 className="title">Sophie Homer </h1>
       <h2 className="introText">Product designer with a background <br></br>in software development.</h2>
       <p className="aboutText">
-        Previously, I worked in costume design where I discovered a love for visual storytelling. Now, storytelling is what inspires my product design. In my work, I strive to find balance between form and function, focusing on detail while never letting the broader picture out of sight. I find joy in making things both beautiful and accessible. On my free time, I like to cook, curate playlists, and catch up on my favorite shows.
-        </p>
+      Previously, I worked in costume design where I discovered a love for visual storytelling. Now, storytelling is what inspires my product design. In my work, I strive to find balance between form and function, focusing on detail while never letting the broader picture out of sight. I find joy in making things both beautiful and accessible. I’m optimistic, curious and excited to further develop my skills. I love working with others, sharing ideas, and learning from those around me.        </p>
       </div>
       <Navigation/>
     </section>
