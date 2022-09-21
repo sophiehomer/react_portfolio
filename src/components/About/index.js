209d06b1c95ef6fcb422dd837685e4d4d0a38284
sophie homer <sophiehomer94@gmail.com>
2022-09-21 11:48:26 -0700

    edited about and work summary

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,13 @@ const About = () => {
       <h1 className="title">Sophie Homer </h1>
       <h2 className="introText">Product designer with a background <br></br>in software development.</h2>
       <p className="aboutText">
-      Previously, I worked in costume design where I discovered a love for visual storytelling. Now, storytelling is what inspires my product design. In my work, I strive to find balance between form and function, focusing on detail while never letting the broader picture out of sight. I find joy in making things both beautiful and accessible. I’m optimistic, curious and excited to further develop my skills. I love working with others, sharing ideas, and learning from those around me.        </p>
+      I discovered a love for visual storytelling while working in costume design for film and television. Now, I use that foundational love for storytelling to inspire my product design.
+
+In my work, I strive to find balance between form and function, focusing on detail while keeping the whole canvas in mind… making things both beautiful and accessible.
+
+I’m optimistic, curious, and eager to develop my skills. I love working with others, sharing ideas, and learning from those around me.
+      {/* Previously, I worked in costume design where I discovered a love for visual storytelling. Now, storytelling is what inspires my product design. In my work, I strive to find balance between form and function, focusing on detail while never letting the broader picture out of sight. I find joy in making things both beautiful and accessible. I’m optimistic, curious and excited to further develop my skills. I love working with others, sharing ideas, and learning from those around me.         */}
+      </p>
       </div>
       <Navigation/>
     </section>
--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -11,29 +11,27 @@ const Work = () => {
            <NavProject/>
         <section className='workIntroContainer'>
         <p className="workIntro"> 
-            In 2018 I went on a mission to find intimate soap. I remember walking down the aisles and being disappointed. The options were limited, the aesthetic was cold and corporate, and the ingredients consisted of harsh chemicals. I went home and began the search for intimate soap online. To my surprise, there were few options and I wasn’t impressed with any of them. I decided to start my own feminine hygiene company and I would call it Femme Dept. 
-        </p>
+        I went on a mission to find a high-quality, organic intimate soap. The options were limited; the aesthetics cold and corporate; and the ingredients often harsh. As I studied the labels, I felt myself become embarrassed and insecure. It was that moment that compelled me to create a joyful user experience for others. I decided to start my own feminine hygiene company: Femme Dept.   
+         </p>
         <p className="workIntro"> 
-            I began my research journey and started having conversations with my mentor. He would tell me to stand in the aisle and evaluate how I felt when I looked at the intimate soaps. I thought the suggestion was a bit bizarre but I did it anyway. As I stood in the aisle studying the labels I noticed I was feeling embarrassed and insecure. From that point on, I wanted Femme Dept. to empower women. From purchasing the product to displaying it in the shower, I wanted to create a joyful user experience.
-        </p>
+        I hired a chemist and a designer. The only way to reach potential customers and convince them that Femme Dept. was a superior product was through an impactful website. Despite my designer’s strong technical skills, there was a disconnect around aesthetics. This led me to want to be more hands on and do things on my own.       
+         </p>
         <p className="workIntro">
-            I hired a chemist and a designer and the ball began to roll. Because I wanted to sell the product online initially, the website was very important to me. My web designer had strong technical skills but there was a disconnect with the aesthetic I wanted to achieve. This led me to want to do it on my own. I had no experience but I was determined to learn.
-        </p>
+        Midway through the process, the chemist I hired passed away — this unfortunate news helped me decide to focus exclusively on learning and put Femme Dept. development on pause. </p>
         <p className="workIntro">
-            In May of 2022 my chemist passed away from Covid, and I decided to shelve this idea for the time being.  My yearn to learn only grew afterwards. In December of 2022 I enrolled myself in a full-stack coding bootcamp. Although I wanted to be a designer, I felt it was important to have a firm grasp on function. 
-        </p>
+        To gain a firm grasp on function as a designer, I enrolled in a full-stack coding bootcamp. I learned how to code responsive applications, design user interfaces, and research efficiently.        </p>
         <p className="workIntro">
-            Throughout my program I learned how to code responsive applications from start to finish, design user interfaces, and research efficiently. I'm looking to grow as a designer and providing pleasurable experiences for others is what motivates me.
-        </p>
+        Now, I’m eager to grow as a designer and excited to build delightful experiences.
+       </p>
 
-        <p className="workIntro">Below are a few projects displaying my skillset</p>
+        <p className="workIntro">Below are a few projects displaying my skillsets:</p>
         </section>  
        
         <Portfolio/>
       </div>
     )
   }
-  
+   
   export default Work
 
 
