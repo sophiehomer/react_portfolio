706ce8e071ed6834ab4b51ca3aa8d4b7b0c9c7b3
sophie homer <sophiehomer94@gmail.com>
2022-09-26 19:36:27 -0700

    added color, modified css

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -15,7 +15,7 @@ const About = () => {
         I enjoy developing a story around a line of products or services and using web design as a vehicle to tell that story in a way that emotionally touches the consumer.  
         I appreciate the fast pace at which web design evolves. I value work that allows me to draw on multiple creative solutions around a single project.
         </p>
-        <h2 className="second"> What sets me apart</h2>
+        <h2 className="second"> What sets me apart?</h2>
         <p className="secondSummary">
           I spent several years in costume design and have a strong background in software development. This combination affords me a unique and effective skillset for web design.  
         </p>
@@ -23,9 +23,9 @@ const About = () => {
           Costume design demands a broad and deep familiarity with the history and principles of design. It also requires strong research skills, which I relied on for character development, historical accuracy, and audience tastes and preferences.
         </p>
         <p className="secondSummary">
-          As a software developer, I’m able to code an application from start to finish. I’m dedicated to detail and a practical and creative problem solver. I’m also fluent in the language of engineers and developers, which allows me to translate effectively among technical, content, and administrative teams and ensure that client expectations are met or exceeded.
+          As a software developer, I’m able to code an application from start to finish. I’m dedicated to detail and a practical and creative problem solver. I’m also fluent in the language of engineers and developers, which allows me to translate effectively among technical, content, and administrative teams and ensure that expectations are met or exceeded.
         </p>
-        <h3 className="third">What I'm looking for in a company</h3>
+        <h3 className="third">What am I looking for in a company?</h3>
         <p className="thirdSummary">
           I hope to find a company that affords me the opportunity to learn from and grow with others, to solve problems collaboratively, to add value, and to generate innovative design of which I can be proud.
         </p>
@@ -67,13 +67,14 @@ const About = () => {
           </p>
           </div>
 
-          {/* <div className="personalSkills">
-          <p className="fourth">Personal</p>   
+          <div className="personalSkills">
+          <p className="personal">Personal</p>   
           <p className="fourthSummary">
             Collaboration <br></br>
             Storytelling <br></br>
+            Leadership
           </p>
-          </div> */}
+          </div>
 
 
         </div>
